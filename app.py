ea67d4460dbc054403f010ee71552efe1e586c7a
stavilavlad <vladstavila@gmail.com>
2023-10-27 17:56:28 +0300

    started database and route to chest

--- app.py
+++ app.py
@@ -9,7 +9,29 @@ from helpers import apology, login_required, lookup, usd
 
 app = Flask(__name__)
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+# db = SQL("sqlite:///finance.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 
 @app.route("/")
 def index():
-    return render_template("layout.html")
+    return render_template("index.html")
+
+
+@app.route("/chest")
+def chest():
+    return render_template("chest.html")
